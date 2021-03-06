0450e882625b68820d08f9d9019bd67b466e0ab6
HOUDASS Youness <youness.houdass@sicpa.com>
2021-03-06 09:48:08 +0100

    1. Introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MessageService} from './message.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'services-examples';
+  message: string;
+  constructor(private messageService: MessageService) {
+    this.message = this.messageService.getMessage();
+  }
 }
---
+++ src/app/message.service.ts
@@ -0,0 +1,5 @@
+export class MessageService {
+  getMessage(): string {
+    return 'Message From MessageService';
+  }
+}
